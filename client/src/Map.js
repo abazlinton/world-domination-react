7c6e42f972d4d7f9fe72f5423c788df3f16f0d4a
Alex Bazlinton <abazlinton@gmail.com>
2017-05-29 22:25:26 +0100

    basic map setup

--- client/src/Map.js
+++ client/src/Map.js
@@ -1,9 +1,26 @@
+/* global google */
 import React, {Component} from 'react';
 
 class Map extends Component {
 
+  constructor(props) {
+    super(props);
+  }
+
+  onComponentDidMount(){
+    this.googleMap = new google.maps.Map(this.refs.map, {
+      center: {lat: 0, lng: 0}
+    })
+  }
+
   render() {
-    return(<div>map</div>)
+    return(
+      <div 
+        ref="map"
+      >
+
+      </div>
+    )
   }
 
 }
--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,11 @@ import Map from './Map';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
